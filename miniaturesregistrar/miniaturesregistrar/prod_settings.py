6c59164056d5088e13c908484daae30a56170e04
Paweł Marczewski <pwmarcz@gmail.com>
2017-02-07 23:01:38 +0100

    Override more URLs in prod settings

--- miniaturesregistrar/miniaturesregistrar/prod_settings.py
+++ miniaturesregistrar/miniaturesregistrar/prod_settings.py
@@ -19,4 +19,7 @@ DATABASES = {
 ALLOWED_HOSTS = ['178.62.22.84', 'polandfinlandplayground.org']
 
 STATIC_URL = '/mini/static/'
-MEDIA_URL = '/mini/media/'
+MEDIA_URL = '/mini/images/'
+
+LOGIN_URL = '/mini/login/'
+LOGIN_REDIRECT_URL = '/mini/'
